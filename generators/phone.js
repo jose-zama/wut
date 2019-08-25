a5c145c66c030e081735ed42861d604e0ccf142a
Brito Mendoza, Luis Alberto <luisalberto.britomendoza@thermofisher.com>
2019-08-24 20:37:39 -0700

    Cambiando todo a Index.js

--- generators/phone.js
+++ generators/phone.js
@@ -1,10 +1,1 @@
 var faker = require('faker');
-
-var generate = function(){
-    return faker.helpers.replaceSymbolWithNumber("## COMPRA T AIRE ########## EN BPI");
-}
-
-
-module.exports = {
-    generate: generate
-}
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var faker = require('faker');
 
-var phone = require("./generators/phone")
+const randomNumberWith = (length) => {    
+    return faker.helpers.replaceSymbolWithNumber("#".repeat(length));
+};
+
+const phone = () => {
+    return `${randomNumberWith(2)} COMPRA T AIRE ${randomNumberWith(10)} EN BPI, Bills & Utilities`;
+}
+
+const volaris = () => {
+    return `VOLARIS ECOM ${faker.finance.currencyCode()} MEX, Travel`;
+}
+
+const 
 
 for (var i = 0; i < 1000; i++) {
-    console.log(phone.generate());
+    console.log(phone());
+    console.log(volaris());
 }
